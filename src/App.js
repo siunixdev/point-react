e49ce7d448251b97a884e85cb60a342178ffac11
siunix <abd.siunix@gmail.com>
2022-02-21 21:49:31 +0700

    Update: layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='grid grid-flow-col h-screen grid-cols-12 gap-2'>
+      <div className='sm:flex flex-col justify-end bg-slate-400 hidden col-span-12 sm:col-span-7 md:col-span-7 lg:col-span-8 pl-20 pb-20'>
+        <span className='text-3xl mb-3'>Make it Easy!</span>
+        <span className='text-5xl font-bold'>Make it Simple</span>
+      </div>
+      <div className='flex flex-col bg-slate-500 col-span-12 sm:col-span-5 md:col-span-5 lg:col-span-4'>
+        <span>POINT</span>
+        <span>Login</span>
+        <span>Login to your Account</span>
+        <span>Thanks you for get back to Point System, Let's Access for now!</span>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
